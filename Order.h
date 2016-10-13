187f1d7271d61f73017099e1ebcac9fa445b856f
Eric <Quisk@users.noreply.github.com>
2016-10-12 21:41:51 -0500

    Add operator<< and update printOrder()
    Added the operator<< and changed printOrder() to return a string rather than use cout.

--- Order.h
+++ Order.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include "Date.h"
 #include <list>
@@ -11,6 +12,7 @@ using namespace std;
 
 class Order
 {
+	friend ostream& operator<<(ostream& out, Order & rightOrder);
 public:
 	Order();
 	Order(string orderID, Date orderDate, Date deliveryDate);
@@ -19,7 +21,7 @@ public:
 	bool isOrder(string inStr);
 	bool operator==(Order& rhs);
 	bool operator<(Order& rhs);
-	void printOrder();
+	string printOrder();
 	void addOrderItem(const orderItem& inItem);
 	void removeOrderItem(orderItem& inItem);
 	string getOrderID();
